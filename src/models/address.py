e00b4eb494f15aac7e610f11595e8b4f93431b98
head1ton <head1ton@gmail.com>
2022-07-19 13:11:52 +0900

    model 생성

---
+++ src/models/address.py
@@ -0,0 +1,32 @@
+from time import time
+from uuid import uuid4
+
+from pydantic import Field
+
+
+__all__ = ("Address")
+
+_string = dict(min_length=1)
+
+
+class Address:
+    street = Field(
+        description="Main address line",
+        example="22nd Bunker Hill Avenue",
+        **_string
+    )
+    city = Field(
+        description="City",
+        example="Hamburg",
+        **_string
+    )
+    state = Field(
+        description="State, province and/or region",
+        example="Mordor",
+        **_string
+    )
+    zip_code = Field(
+        description="Postal/ZIP code",
+        example="19823",
+        **_string
+    )
--- src/models/person.py
+++ src/models/person.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from pydantic import Field
 
 
-__all__ = ("Person")
+__all__ = ("Person", )
 
 _string = dict(min_length=1)
 
@@ -47,27 +47,4 @@ class Person:
         alias="updated",
         description="When the person was updated for the last time (Unix timestamp)",
         **_unix_ts
-    )
-
-
-class Address:
-    street = Field(
-        description="Main address line",
-        example="22nd Bunker Hill Avenue",
-        **_string
-    )
-    city = Field(
-        description="City",
-        example="Hamburg",
-        **_string
-    )
-    state = Field(
-        description="State, province and/or region",
-        example="Mordor",
-        **_string
-    )
-    zip_code = Field(
-        description="Postal/ZIP code",
-        example="19823",
-        **_string
     )
